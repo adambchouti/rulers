5e6603d81140ea8f2d6cb417be44f026bfb3a2c0
Adam <adambchouti93@gmail.com>
2026-06-18 22:58:54 -0400

    Added the 3 small exercises for chap-1

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "rulers/version"
+require "rulers/array"
 
 module Rulers
   # Basica application that starts a Rack server
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, { "content-type" => "text/html" }, ["Hello from Ruby on Rulers!"]]
     end
   end
