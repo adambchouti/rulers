c649fbb65e858f8ae7048ac007bcd9eaf405c57c
Adam <adambchouti93@gmail.com>
2026-06-20 19:30:38 -0400

    Adding very basic routing system

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 
 require_relative "rulers/version"
+require_relative "rulers/routing"
 
 module Rulers
   # Basica application that starts a Rack server
   class Application
     def call(env)
-      `echo debug > debug.txt`;
-      [200, { "content-type" => "text/html" }, ["Hello from Ruby on Rulers!"]]
+
+      # Get a controller and an action from the URL
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+
+      [200, { "content-type" => "text/html" }, [text]]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
